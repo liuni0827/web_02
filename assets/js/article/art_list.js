b3614a341d65524ca799b1e271c4e53c71d28f8c
liunini <tianshanlian1010@163.com>
2020-10-15 18:14:01 +0800

    完成了个人中心模块的开发

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,61 @@
+$(function () {
+    var q = {
+        pagenum: 1,
+        pagesize: 2,
+        cate_id: '',
+        state: ''
+    }
+    initTable()
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                layer.msg('获取文章列表成功！')
+            }
+        })
+    }
+
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
+    initCate()
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                var htmlStr = template('tpl-cate', res)
+
+            }
+        })
+    }
+
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault()
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.cate_id = cate_id
+        q.state = state
+        initTable()
+    })
+})
