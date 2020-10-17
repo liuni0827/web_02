325535936605d542e370740e06311a7c4237b12a
liunini <tianshanlian1010@163.com>
2020-10-17 14:53:08 +0800

    完成了大事件项目的开发

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -3,21 +3,24 @@ $(function () {
     var form = layui.form
     initArtCateList()
 
-function initArtCateList(){
-    $.ajax({
-        method:'GET',
-        url: '/my/article/cates',
-        
-    })
-}
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
     var indexAdd = null;
-    $('btnAddCate').on('click', function () {
+    $('#btnAddCate').on('click', function () {
         indexAdd = layer.open({
-            type：1,
+            type: 1,
             area: ['500px', '250px'],
-            title: '添加文章分类！'
-            , content: $('#dialog-add').html()
-        });
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        })
     })
     $('body').on('submit', '#form-add', function (e) {
         e.preventDefault()
@@ -26,11 +29,12 @@ function initArtCateList(){
             url: '/my/article/addcates',
             data: $(this).serialize(),
             success: function (res) {
-                if (res.stutas !== 0) {
+                if (res.status !== 0) {
                     return layer.msg('新增分类失败！')
                 }
                 initArtCateList()
                 layer.msg('新增分类成功！')
+                // 根据索引，关闭对应的弹出层
                 layer.close(indexAdd)
             }
         })
@@ -38,12 +42,13 @@ function initArtCateList(){
 
     var indexEdit = null
     $('tbody').on('click', '.btn-edit', function () {
+        // 弹出一个修改文章分类信息的层
         indexEdit = layer.open({
-            type：1,
+            type: 1,
             area: ['500px', '250px'],
-            title: '添加文章分类！'
-            , content: $('#dialog-edit').html()
-        });
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        })
         var id = $(this).attr('data-id')
         var name = $(this).attr('data-name')
         var alias = $(this).attr('data-alias')
@@ -53,28 +58,31 @@ function initArtCateList(){
             alias: alias
         })
     })
-    $('body').on('submit','#form-edit',function(e){
+    $('body').on('submit', '#form-edit', function (e) {
         e.preventDefault()
         $.ajax({
-            method:'POST',
-            url:'/my/article/updatecate',
-            data:$(this).serialize(),
-            success:function(res){
-                if(res.stutas!==0){
-                    return layer.msg('')
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新分类数据失败！')
                 }
+                layer.msg('更新分类数据成功！')
+                layer.close(indexEdit)
+                initArtCateList()
             }
         })
     })
 
-    $('tbody').on('click','.btn-delete',function(){
+    $('tbody').on('click', '.btn-delete', function () {
         var id = $(this).attr('data-id')
-        layer.confirm('确认删除？',{icon:3,title:'提示'},function(index){
+        layer.confirm('确认删除？', { icon: 3, title: '提示' }, function (index) {
             $.ajax({
-                method:'GET',
-                url:'/my/article/deletecate'+id,
-                success:function(res){
-                    if(res.stutas!==0){
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
                         return layer.msg(res.message)
                     }
                     layer.msg('删除分类成功！')
--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,11 +1,16 @@
 $(function () {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
     var q = {
         pagenum: 1,
         pagesize: 2,
         cate_id: '',
         state: ''
     }
+    var curretData = null
     initTable()
+    initCate()
 
     function initTable() {
         $.ajax({
@@ -16,10 +21,14 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg('获取文章列表失败！')
                 }
-                layer.msg('获取文章列表成功！')
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
+                curretData = res.data
             }
         })
     }
+
 
     template.defaults.imports.dataFormat = function (date) {
         const dt = new Date(date)
@@ -35,7 +44,6 @@ $(function () {
         return n > 9 ? n : '0' + n
     }
 
-    initCate()
     function initCate() {
         $.ajax({
             method: 'GET',
@@ -45,7 +53,8 @@ $(function () {
                     return layer.msg('获取分类数据失败！')
                 }
                 var htmlStr = template('tpl-cate', res)
-
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
             }
         })
     }
@@ -57,5 +66,54 @@ $(function () {
         q.cate_id = cate_id
         q.state = state
         initTable()
+    })
+
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox',
+            count: total,
+            limit: q.pagesize,
+            curr: q.pagenum,
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            jump: function (obj, first) {
+                // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
+                // 如果 first 的值为 true，证明是方式2触发的
+                // 否则就是方式1触发的
+                console.log(first)
+                console.log(obj.curr)
+                // 把最新的页码值，赋值到 q 这个查询参数对象中
+                q.pagenum = obj.curr
+                // 把最新的条目数，赋值到 q 这个查询参数对象的 pagesize 属性中
+                q.pagesize = obj.limit
+                // 根据最新的 q 获取对应的数据列表，并渲染表格
+                // initTable()
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+
+    $('tbody').on('click', '.btn-delete', function () {
+        var len = $('.btn-delete').length
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除？', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    if (len === 1) {
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+            layer.close(index)
+        })
     })
 })
